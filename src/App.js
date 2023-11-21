33f7288e61ac80ccd066b6bfa2c4f40b40a1b665
Colin Dively <colinblairdively@gmail.com>
2023-11-21 16:12:13 -0500

    basic home page with avatar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+  import './App.css';
+import image from './wizard (2).png';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <a href='/weather'>Weather</a>
+        <img src={image} className="App-logo" alt="logo" />
       </header>
     </div>
   );
